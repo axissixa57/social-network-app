47a4478a65f02a4e52e3d29a3d2e5850edf01a0d
Ax1S <axissixa@mail.ru>
2019-08-18 23:58:15 +0300

    84. hook - useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import object from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
@@ -13,7 +14,8 @@ const ProfileInfo = (props) => {
         <div>
             <div className={object.descriptionBlock}>
                 <img src={props.profile.photos.large} alt=""/>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                {/*ProfileStatus*/}
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     );
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+
+const ProfileStatusWithHooks = (props) => {
+    // hook - используется state и возможность его изменения внутри ф-ции ProfileStatusWithHooks
+    // useState - возращает массив, в кот. первый элемент заданное значение, а 2-ой ф-ция кот. меняет его
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        // отправляем на значение на сервер когда вышли из редактируемого мода
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            {/*true && 1 - покажет 1*/}
+            { !editMode &&
+                <div>
+                    <span onDoubleClick={activateEditMode}>{props.status || 'Hello World!'} yo</span>
+                </div>
+            }
+            {editMode &&
+            <div>
+                {/*onBlur, когда фокус уходит с элемента*/}
+                <input autoFocus={true} onBlur={deactivateEditMode} onChange={onStatusChange} value={status}/>
+            </div>
+            }
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks;
