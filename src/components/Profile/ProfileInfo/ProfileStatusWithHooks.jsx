066b53aab6462ff4171a11489f3b92c1ccdfa6c2
Ax1S <axissixa@mail.ru>
2019-08-20 21:42:17 +0300

    85. useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ProfileStatusWithHooks = (props) => {
     // hook - используется state и возможность его изменения внутри ф-ции ProfileStatusWithHooks
@@ -6,26 +6,33 @@ const ProfileStatusWithHooks = (props) => {
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
 
+    // синхронизируем данными (синхронизация состояния) кот. пришли из пропросов: 1 раз придёт, пустая строка, 2 раз после того как компонента вмонтировалась, сработает useEffect (наподобии componentDidMound, также в useEffect работает componentDidUpdate)
+    // [] массив нужен для того чтобы отрисовать один раз после вмонтирования, а значение в массиве, говорит что мы зависим от props.status, для обновления (componentDedUpdate),
+    // если статус был не таким как раньше, то сработает useEffect
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
+
     const activateEditMode = () => {
         setEditMode(true);
-    }
+    };
 
     const deactivateEditMode = () => {
         setEditMode(false);
         // отправляем на значение на сервер когда вышли из редактируемого мода
         props.updateStatus(status);
-    }
+    };
 
     const onStatusChange = (e) => {
         setStatus(e.currentTarget.value);
-    }
+    };
 
     return (
         <div>
             {/*true && 1 - покажет 1*/}
             { !editMode &&
                 <div>
-                    <span onDoubleClick={activateEditMode}>{props.status || 'Hello World!'} yo</span>
+                    <span onDoubleClick={activateEditMode}>{props.status || 'Hello World!'}</span>
                 </div>
             }
             {editMode &&
