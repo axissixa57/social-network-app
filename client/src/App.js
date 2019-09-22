9f64a13951d84fe4c51df03c7c1790d208c87ac6
Ax1S <axissixa@mail.ru>
2019-09-22 21:28:52 +0300

    feat: add React.lazy and React.Suspense to ыsome components in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import {Route, withRouter} from 'react-router-dom';
+import {connect} from "react-redux";
+import {compose} from "redux";
+
+import {initializeApp} from "./redux/reducers/app";
+
 import './App.css';
+
 import Navbar from './components/Navbar/Navbar';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import {connect} from "react-redux";
-import {compose} from "redux";
-import {initializeApp} from "./redux/reducers/app";
 import Preloader from "./components/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -31,9 +36,9 @@ class App extends Component {
                     {/*роутер связан с NavLink компонентой, кот. наход-ся в Navbar*/}
                     {/*роутер следит за изменением url, если он изменяется он отрисовывет, то что ему назначено*/}
                     {/*'/profile/:userId?' знак ? говорит что параметр не обязателен, и отрисует странницу, без знака ничего не отрисует*/}
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
                     {/* exact необоходит для точного совпадения, чтобы при клике /dialogs/1 не подгружался компонент Dialogs */}
-                    <Route exact path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route exact path='/dialogs' render={withSuspense(DialogsContainer)}/>
                     <Route path='/users' render={() => <UsersContainer/>}/>
                     <Route path='/login' render={() => <Login/>}/>
                     <Route path='/register' render={() => <Register/>}/>
