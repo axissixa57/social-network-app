70eed87e9897d0770ed327502d699a3b1ffca106
Ax1S <axissixa@mail.ru>
2019-09-22 21:57:29 +0300

    Updates

---
+++ precache-manifest.2cd16af7df55330f3ff037e4898f7a1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4b2983ad6a2e09d91a6908433970ee",
+    "url": "/social-network-app/index.html"
+  },
+  {
+    "revision": "3376fbd1042188d74fd8",
+    "url": "/social-network-app/static/css/3.469746b5.chunk.css"
+  },
+  {
+    "revision": "90f29a690f20b0a46e65",
+    "url": "/social-network-app/static/css/4.0d1b0a38.chunk.css"
+  },
+  {
+    "revision": "2b7ce53a3d974f4796e8",
+    "url": "/social-network-app/static/css/main.89731dc6.chunk.css"
+  },
+  {
+    "revision": "e222736afe1a169d1b9f",
+    "url": "/social-network-app/static/js/2.b3ff4aa0.chunk.js"
+  },
+  {
+    "revision": "3376fbd1042188d74fd8",
+    "url": "/social-network-app/static/js/3.30319882.chunk.js"
+  },
+  {
+    "revision": "90f29a690f20b0a46e65",
+    "url": "/social-network-app/static/js/4.fee4eb49.chunk.js"
+  },
+  {
+    "revision": "2b7ce53a3d974f4796e8",
+    "url": "/social-network-app/static/js/main.9b6f6419.chunk.js"
+  },
+  {
+    "revision": "277f5092ad55c412b74b",
+    "url": "/social-network-app/static/js/runtime~main.85db9228.js"
+  },
+  {
+    "revision": "7e094df1d988e66392e9ee75d38a8ba7",
+    "url": "/social-network-app/static/media/pavel-durov.7e094df1.jpg"
+  },
+  {
+    "revision": "fbb95c93dc32580ff31f669dc5e5043c",
+    "url": "/social-network-app/static/media/preloader.fbb95c93.svg"
+  }
+]);
